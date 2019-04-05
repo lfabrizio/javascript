21181e7c3ed34ec39473a594d6ad2a946327d981
Louis fabrizio <louisfabrizio.student@careerdevs.com>
2019-04-05 20:30:29 +0000

    first commit

---
+++ if.js
@@ -0,0 +1,20 @@
+
+// Nothing Complex but testing out how I can test out a scoring system with If functions.
+
+
+function GradingSystem(score) {
+   if (score < 65) {
+       return "Failed";
+   }
+
+   else {
+       return "Passed";
+   }
+}
+
+
+GradingSystem(57)
+GradingSystem(90)
+
+
+//Did this from scratch without referring to any help. Therefore, proud of the progress.
